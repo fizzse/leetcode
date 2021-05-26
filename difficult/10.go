851688b71ff72491d5ddc1b1b09be924a9645af6
pines <dualpines@163.com>
2021-05-26 19:07:26 +0800

    diffcult

---
+++ difficult/10.go
@@ -0,0 +1,16 @@
+package main
+
+/*
+给你一个字符串 s 和一个字符规律 p，请你来实现一个支持 '.' 和 '*' 的正则表达式匹配。
+
+'.' 匹配任意单个字符
+'*' 匹配零个或多个前面的那一个元素
+所谓匹配，是要涵盖 整个 字符串 s的，而不是部分字符串。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/regular-expression-matching
+*/
+
+func isMatch(s string, p string) bool {
+	return false
+}
