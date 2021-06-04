1fbde3b7a6bdfdfcc0c6941d489d14ba088e6d0e
pines <dualpines@163.com>
2021-06-04 10:38:19 +0800

    tree

--- simple/160.go
+++ simple/160.go
@@ -82,13 +82,13 @@ func getIntersectionNode(headA, headB *ListNode) *ListNode {
 
 	h1, h2 := headA, headB
 	for h1 != h2 {
-		if h1.Next == nil {
+		if h1 == nil {
 			h1 = headB
 		} else {
 			h1 = h1.Next
 		}
 
-		if h2.Next == nil {
+		if h2 == nil {
 			h2 = headA
 		} else {
 			h2 = h2.Next
